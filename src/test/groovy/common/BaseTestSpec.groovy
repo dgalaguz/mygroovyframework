63603841b39a2ff0314fcd6d264d91b64c13e4cd
metal-d <metal_man96@mail.ru>
2018-01-18 23:58:05 +0200

    improving screenshot reporting(prototype).

--- src/test/groovy/common/BaseTestSpec.groovy
+++ src/test/groovy/common/BaseTestSpec.groovy
@@ -1,19 +1,23 @@
 package common
 
+import com.athaydes.spockframework.report.SpecInfoListener
 import geb.spock.GebReportingSpec
 import groovy.util.logging.Slf4j
 import spock.lang.IgnoreIf
 import spock.lang.Shared
+import sun.security.provider.SHA
 
 @Slf4j
 class BaseTestSpec extends GebReportingSpec{
     @Shared def specName =  specificationContext.currentSpec.name
+    @Shared def listener = specificationContext.currentSpec.listeners.find{it.class == SpecInfoListener}
 
     def setup(){
         log.info("Starting...")
     }
 
     def cleanupSpec(){
+        def runs = listener.specData.featureRuns
         def specFolder = new File(browser.config.reportsDir.path + "\\specs\\" + specName)
         def screenshots = specFolder.listFiles().findAll {it.name.endsWith("failure.png")}
         if(screenshots){
