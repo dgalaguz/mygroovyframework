e1bada30ae09f6d57ec9e55d41d923dd3cf9a626
metal-d <metal_man96@mail.ru>
2018-01-18 23:05:33 +0200

    basic screenshot reporting added.

--- src/test/groovy/common/BaseTestSpec.groovy
+++ src/test/groovy/common/BaseTestSpec.groovy
@@ -2,10 +2,21 @@ package common
 
 import geb.spock.GebReportingSpec
 import groovy.util.logging.Slf4j
+import spock.lang.IgnoreIf
+import spock.lang.Shared
 
 @Slf4j
 class BaseTestSpec extends GebReportingSpec{
+    @Shared def specName =  specificationContext.currentSpec.name
+
     def setup(){
         log.info("Starting...")
+    }
+
+    def cleanupSpec(){
+        def specFolder = new File(browser.config.reportsDir.path + "\\specs\\" + specName)
+        def screenshots = specFolder.listFiles().findAll {it.name.endsWith(".png")}
+        reportHeader("Screenshots:")
+        screenshots.eachWithIndex {item, index -> reportHeader('<a href='+ item.toURI() +'>'+ 'screenshot '+ (index+1) +'</a>')}
     }
 }
